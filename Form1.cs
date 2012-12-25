f387f6a3ae634367f9d69d755eb9cdd82db534db
Mariusz Zieliński <nedbee@live.com>
2012-12-25 13:18:57 +0100

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Timer = System.Threading.Timer;
+
+namespace CapsLockIndicator
+{
+    public partial class Form1 : Form
+    {
+        private const int Offset = 120;
+        private readonly Action _hideAction, _showAction;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _hideAction = new Action(() => Visible = false);
+            _showAction = new Action(() =>
+                                       {
+                                           SetPosition();
+                                           Visible = true;
+                                       });
+
+            var checkCapsTimer = new Timer(delegate { CheckCaps(); });
+            checkCapsTimer.Change(0, 10);
+        }
+
+        private void SetPosition()
+        {
+            // move to lower right corner 
+            var screenSize = SystemInformation.VirtualScreen;
+            Location = new Point(screenSize.Width - Offset, screenSize.Height - Offset);
+        }
+
+        private void CheckCaps()
+        {
+            if (IsKeyLocked(Keys.CapsLock) && Visible)
+                return;
+
+            var action = !IsKeyLocked(Keys.CapsLock)
+                             ? _hideAction
+                             : _showAction;
+
+            if (InvokeRequired)
+                Invoke(action);
+            else 
+                action.Invoke();
+        }
+    }
+}
